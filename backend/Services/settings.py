e0fb21cfed6fbae168d4e50db4f47b0da9e1c50c
joelcampoverde <118577957+joelcampoverde@users.noreply.github.com>
2025-09-12 19:09:13 -0500

    Update settings.py

--- backend/Services/settings.py
+++ backend/Services/settings.py
@@ -153,3 +153,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization"
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
